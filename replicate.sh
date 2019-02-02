2a842872abfb529bd1950bd8534278d455489b1a
Charlie Root <emmanueltimmy98@gmail.com>
2019-02-02 18:46:16 +0000

    Added relayd config and fixed ksh script for fixing certs

--- replicate.sh
+++ replicate.sh
@@ -14,15 +14,8 @@ openssl req -nodes -new -x509 -sha256 -key server.key -out server.crt -days 3650
 
 
 # Move certificates to ssl folder and rename to domain
-CERT_PATH=/etc/ssl/private
-ADDRESS=$1
-TLS_PORT=$2
-mkdir -p $CERT_PATH
-
-cat server.crt server.key > server.includesprivatekey.pem
-cp server.crt $CERT_PATH/$1:$2.crt
-cp server.key $CERT_PATH/$1:$2.key
-cp server.includesprivatekey.pem $CERT_PATH/$1:$2.pem
+# This has been modified in certs.sh
+ksh certs.sh
 
 
 
